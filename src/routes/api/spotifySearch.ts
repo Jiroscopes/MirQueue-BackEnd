00467b70a3c04b5c589b50d5c036e161bc9c0a0a
Steven <steven@Stevens-MacBook-Pro.local>
2022-06-20 22:04:45 -0500

    Fixing stale server creds

--- src/routes/api/spotifySearch.ts
+++ src/routes/api/spotifySearch.ts
@@ -2,13 +2,14 @@ import express from 'express';
 import config from '../../config';
 import Request from '../../Request';
 import { doesSessionExist } from '../../db/session'
+import { getClientCreds } from '../../ClientCreds';
 
 const router = express.Router();
 
-router.post('/', search);
+router.post('/', searchHandler);
 
 // Take a string and return any data
-async function search(req: any, res: any) {
+async function searchHandler(req: any, res: any) {
     
     // User can be a guest
     if (!req.body.username || !req.body.searchParam || !req.body.sessionCode || !req.body.host) {
@@ -24,6 +25,11 @@ async function search(req: any, res: any) {
         return;
     }
 
+    let searchResults = await search(searchParam);
+    return searchResults ? res.json(searchResults) : res.sendStatus(400);
+}
+
+async function search(searchParam: string, secondTry: boolean = false) {
     const searchLimit = 5;
 
     let request = new Request({
@@ -37,10 +43,20 @@ async function search(req: any, res: any) {
             Authorization: `Bearer ${config.client_creds.access_token}`
         },
     });
-
-    let searchRes: any = await request.execute();
-
-    res.json(searchRes['tracks']['items']);
+    
+    try {
+        let searchRes: any = await request.execute();
+        return searchRes['tracks']['items'];
+    } catch (error) {
+        // If already retried, something else is wrong
+        if (secondTry) {
+            return false;
+        }
+        // Get new creds and try again once
+        let creds = await getClientCreds();
+        config.client_creds = creds;
+        search(searchParam, true);
+    }
 }
 
 export default router;
